f2a21996b36d1d0c52e5a2782242032a0850a1c2
patriciajorge <patriciajorge313@gmail.com>
2024-02-19 19:25:23 -0300

    configurando banco de dados

--- projeto_invista_me/settings.py
+++ projeto_invista_me/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'projeto_invista_me.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres', 
+        'USER': 'postgres.oymiaktclyexorrxxlbr', 
+        'PASSWORD': '84aLb6EqXYskD7yc',
+        'HOST': 'aws-0-us-west-1.pooler.supabase.com', 
+        'PORT': '5432'
     }
 }
 
